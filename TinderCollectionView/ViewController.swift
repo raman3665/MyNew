5747ef361bbfcab3de541560a525d610e5685691
Aseem 20 <aseem20@cbl77.local>
2016-04-13 11:32:16 +0530

    Initial Commit

---
+++ TinderCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderCollectionView
+//
+//  Created by Aseem 20 on 13/04/16.
+//  Copyright © 2016 Aseem 20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
